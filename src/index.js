60b4ca389946f7fbdffbfcd047d2a571d3a7db1b
SelectPerson <vovalutsk961@gmail.com>
2023-06-29 15:56:11 +0300

    "some changed v4"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import {QueryClient, QueryClientProvider} from "react-query";
-import {BrowserRouter, createBrowserRouter} from "react-router-dom";
 import App from "./App";
 import './styles/main.scss'
 
